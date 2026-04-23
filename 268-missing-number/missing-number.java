f7fdd30d970dec9dacd6f94c2bb639fa8fc10500
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-04-23 20:05:46 +0530

    Time: 7 ms (16.43%) | Memory: 47.4 MB (51.27%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -2,11 +2,23 @@ class Solution {
     public int missingNumber(int[] nums) {
 
         //sum of n natural no.
-        
+        HashSet<Integer> set = new HashSet<>();
         int n = nums.length;
-        int expected = n * (n + 1) / 2;
+
+        for(int ele : nums){
+            set.add(ele);
+        }
+
+        for(int i=0; i<=n; i++){
+            if(set.contains(i) == false){
+                return i;
+            }
+        }
+        
+    /*    int expected = n * (n + 1) / 2;    // 1 SC   N-TC
         int sum = 0;
         for (int num : nums) sum += num;
-        return expected - sum;
+        return expected - sum;             */
+        return -1;
     }
 }
