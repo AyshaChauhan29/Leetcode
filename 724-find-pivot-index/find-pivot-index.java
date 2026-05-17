114c89174c328ef6a8951dd855e70b59b1b2871b
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-05-17 14:25:08 +0530

    Time: 1 ms (98.49%) | Memory: 47.4 MB (58.36%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,40 +1,24 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-    /*    int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
+       int n = nums.length;
 
-        int leftSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            // rightSum = totalSum - leftSum - nums[i]
-            if (leftSum == totalSum - leftSum - nums[i]) {
-                return i;
-            }
-            leftSum += nums[i];
-        }
-
-        return -1;        */
-
-      int n = nums.length;
-      
-      int[] pre = new int[n];     //initially {0, 0, 0, 0}
-      int sum = 0;
-      for(int i=0; i<n; i++){
-        sum = sum + nums[i];
+       int[] pre = new int[n];
+       int sum = 0;
+       for(int i=0; i<n; i++){
+        sum += nums[i];
         pre[i] = sum;
-      }
-      
-      int[] suff = new int[n];
-      sum = 0;                        //no need to declare it again
-      for(int i=n-1; i>=0; i--){
-        sum = sum + nums[i];
+       }
+
+       int[] suff = new int[n];
+       sum = 0;
+       for(int i=n-1; i>=0; i--){
+        sum += nums[i];
         suff[i] = sum;
-      }
-       
-         for(int i=0; i<n; i++) {
-            if(pre[i] == suff[i]) return i;
-         }
+       }
+
+       for(int i=0; i<n; i++){
+          if(suff[i] == pre[i]) return i;
+       }
        return -1;
     }
 }
