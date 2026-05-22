b6994126bf70c2ba28d5f8b2ecdd3d08efa3c9ef
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-05-23 00:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (49.50%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,49 +2,41 @@ class Solution {
         
     public int[] searchRange(int[] nums, int target) {
 
-        int low = 0;
-        int high = nums.length - 1;
-        int fi = -1;
+       int n = nums.length;
+       int low = 0;
+       int high = n-1;
+       int fi = -1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+       while(low <= high){
+         int mid = low + (high-low)/2;
 
-            if (nums[mid] == target) {
-                fi = mid;
-                high = mid - 1; // search on left side
-            } 
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            } 
-            else {
-                high = mid - 1;
-            }
-        }
+         if(nums[mid] == target){
+            fi = mid;
+            high = mid-1;
+         }
+         else if(nums[mid] > target) high = mid-1;
+         else low = mid+1;
+       }
 
+       low = 0;
+       high = n-1;
+       int li = -1;
 
-         low = 0; 
-         high = nums.length - 1;
-        int li = -1;
+       while(low <= high){
+         int mid = low + (high-low)/2;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+         if(nums[mid] == target){
+            li = mid;
+            low = mid+1;
+         }
+         else if(nums[mid] > target) high = mid-1;
+         else low = mid+1;
+       }
 
-            if (nums[mid] == target) {
-                li = mid;
-                low = mid + 1; // search on right side
-            } 
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            } 
-            else {
-                high = mid - 1;
-            }
-        }
+       int[] arr = new int[2];
+       arr[0] = fi;
+       arr[1] = li;
 
-        int[] arr = new int[2];
-        arr[0] = fi;
-        arr[1] = li;
-
-        return arr;
+       return arr;
     }
 }
