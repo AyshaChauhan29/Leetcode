ac529fd06ee5ea48f510aa33d34b40228f85e88b
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-05-24 22:22:13 +0530

    Time: 1 ms (66.46%) | Memory: 46.5 MB (18.66%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.java
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.java
@@ -1,16 +1,19 @@
 class Solution {
     public int averageValue(int[] nums) {
-        int sum = 0;
-        int count = 0;
+       int n = nums.length;
+       int sum = 0;
+       int count = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] % 6 == 0){  
+       for(int i=0; i<n; i++){
+         if(nums[i]%2 == 0){
+            if(nums[i]%3 == 0){
                 sum += nums[i];
                 count++;
             }
-        }
+         }
+       }
 
-        if(count == 0) return 0;
-        return sum / count; 
+       if(sum == 0) return 0;
+       return sum / count;
     } 
 }
