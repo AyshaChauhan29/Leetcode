e6ac4fee646b19eeab2414f569b24c55ca5f3cdd
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-06-03 22:37:14 +0530

    Time: 8 ms (13.53%) | Memory: 49.2 MB (14.39%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,47 +1,52 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    /*    int n = nums1.length;
+        int n = nums1.length;
         int m = nums2.length;
 
         int[] ans = new int[n + m];
 
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < n; i++) {
             ans[i] = nums1[i];
         }
 
-        for (int i = 0; i < b.length; i++) {
+        for (int i = 0; i < m; i++) {
             ans[n + i] = nums2[i];
         }
 
         Arrays.sort(ans);
+        int len  = ans.length;
 
-       for(int i=0; i<ans.length; i++){
-          if(ans.length%2 != 0){
-             return ans[n/2-1];
+       for(int i=0; i<len; i++){
+          if(len%2 == 0){
+             return (ans[len/2-1] + ans[len/2])/2.0;
           }else{
-            return ans[n/2];
+            return ans[len/2];
           }
-       }                   */
+       }                   
+        return 0;
 
-       ArrayList<Integer> li = new ArrayList<>();
 
-       for(int i=0; i<nums1.length; i++){
-        li.add(nums1[i]);
-       }
 
-       for(int i=0; i<nums2.length; i++){
-        li.add(nums2[i]);
-       }
 
-       Collections.sort(li);
-       int n = li.size();
 
-       if(n % 2 == 0){
-         return (li.get(n/2) + li.get(n/2-1))/2.0;
-       }else{
-        return li.get(n/2);
-       }
+    //    ArrayList<Integer> li = new ArrayList<>();
 
+    //    for(int i=0; i<nums1.length; i++){
+    //     li.add(nums1[i]);
+    //    }
+
+    //    for(int i=0; i<nums2.length; i++){
+    //     li.add(nums2[i]);
+    //    }
+
+    //    Collections.sort(li);
+    //    int n = li.size();
+
+    //    if(n % 2 == 0){
+    //      return (li.get(n/2) + li.get(n/2-1))/2.0;
+    //    }else{
+    //     return li.get(n/2);
+    //    }
 
     }
 }
