ef1e3b4409149fa0c82bee8eccadbc1a2f7fdd13
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-06-05 19:47:44 +0530

    Time: 2 ms (95.04%) | Memory: 64.7 MB (90.57%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,43 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+       int n = nums.length;
 
-        //BRUTE FORCE APPROACH -----> Gives TLE
-        // int n = nums.length;
-        // int[] res = new int[n];
+       int[] pre = new int[n];
+         pre[0] = 1;
+         for(int i=1; i<n; i++){
+            pre[i] = pre[i-1] * nums[i-1];
+         }  
 
-        // for(int i=0; i<n; i++){
-        //     int prod = 1;
-        //     for(int j=0; j<n; j++){
-        //         if(j != i){
-        //             prod = prod * nums[j];
-        //         }
-        //     }
-        //     res[i] = prod;
-        // }
-        // return res;
+       int[] suff = new int[n];
+         suff[n-1] = 1;
+         for(int i=n-2; i>=0; i--){
+            suff[i] = suff[i+1] * nums[i+1];
+         }
 
-        int n = nums.length;
-        int[] res = new int[n];
-        int prod = 1;
-
-        int[] pref = new int[n];
-        pref[0] = nums[0];
-        for(int i=1; i<n; i++){
-            pref[i] = pref[i-1] * nums[i];
-        }
-
-        int[] suff = new int[n];
-        suff[n-1] = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            suff[i] = suff[i+1] * nums[i];
-        }
-
-        res[0] = suff[1];
-        res[n-1] = pref[n-2];
-
-        for(int i=1; i<n-1; i++){
-            res[i] = pref[i-1] * suff[i+1];
-        }
-        return res;
+         int[] prod = new int[n];
+         for(int i=0; i<n; i++){
+             prod[i] = pre[i] * suff[i];
+         }
+        return prod;
     }
 }
