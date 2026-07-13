63d63bc8c8453318a9f0890d3fe72af69f482ec6
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-13 13:31:55 +0530

    Time: 13 ms (99.95%) | Memory: 42.1 MB (53.63%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -1,16 +1,19 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+      
 public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        int low = 1;
-        int high = n;
 
-         while(low <= high){
-            int mid = low + (high - low)/2;
-            if (isBadVersion(mid) == false) low = mid + 1;
-            else if(isBadVersion(mid) == true )  high = mid-1;
-         }
-       return low;
+    public int isBad(int low, int high){
+        if(low > high) return low;
+
+        int mid = low + (high - low)/2;
+        if(isBadVersion(mid)) return isBad(low, mid-1);
+        else return isBad(mid+1, high);
+      }
+
+
+    public int firstBadVersion(int n) {
+        return isBad(1, n);
     }
 }
