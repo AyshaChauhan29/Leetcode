8f99d5bc9e55a8f3801c6a6bcfc5991f49f0438d
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-13 23:35:11 +0530

    Time: 1 ms (97.94%) | Memory: 42.9 MB (9.69%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,44 +1,19 @@
 class Solution {
     public int addDigits(int num) {
-        //haashset and recursion -----> best approach
-      /*  while(num >= 10){
-            int sum = 0;
-            while(num > 0){
-                sum += num%10;
-                num /= 10;
-            }
-            num = sum;
-        }
-        return num;          */
-
-
-       //through recursion
-    /*    if(num <= 9){            //base case
-            return num;
-        }
+       HashSet<Integer> set = new HashSet<>();
+       
+        
+    while(num >= 10){
+        if(set.contains(num) == true) break;
+        set.add(num);
 
         int sum = 0;
-        while(num > 0){
-                sum += num%10;
-                num /= 10;
-            }
-       return addDigits(sum);                   */
-
-
-       //THROUGH HASHSET
-       HashSet<Integer> set = new HashSet<>();
-        
-        while (num >= 10) {
-            if (set.contains(num) == true) break;
-            set.add(num);
-            int sum = 0;
-            while (num != 0) {
-                int rem = num % 10;
-                sum = sum + rem;       // add digit (not rem*rem)
-                num = num / 10;
-            }
-            num = sum;
-        }
-        return num;
+       while(num != 0){
+        sum += num%10;
+        num /= 10;
+       }
+       num = sum;
+    }
+      return num;
     }
 }
