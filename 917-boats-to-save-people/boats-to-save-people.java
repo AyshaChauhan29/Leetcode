8720bc3e043c0fe65566a3305463354bea3fce8f
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-13 23:53:30 +0530

    Time: 21 ms (24.57%) | Memory: 56.3 MB (76.13%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,23 +1,22 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        //t.c. => nlogn + o(n)
-       int n = people.length;
-       Arrays.sort(people);
-       int count = 0;
+        int n = people.length;
+        Arrays.sort(people);
+        int count = 0;
 
-       int low = 0;
-       int high = n-1;
+        int low = 0;
+        int high = n-1;
 
-       while(low <= high){
-        if(people[low]+people[high] <= limit){
-            count++;
-            low++;
-            high--;
-        }else {
-            count++;
-            high--;
+        while(low <= high){
+            if(people[low] + people[high] <= limit){
+                count++;
+                low++;
+                high--;
+            }else{
+                count++;
+                high--;
+            }
         }
-       }
-       return count;
+        return count;
     }
 }
