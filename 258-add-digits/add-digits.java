9f9b283327d831642f9a1c42af931873234fbc66
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-13 23:36:26 +0530

    Time: 1 ms (97.94%) | Memory: 42.8 MB (9.69%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,12 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-       HashSet<Integer> set = new HashSet<>();
-       
         
-    while(num >= 10){
-        if(set.contains(num) == true) break;
-        set.add(num);
-
+    while(num > 9){
         int sum = 0;
        while(num != 0){
         sum += num%10;
