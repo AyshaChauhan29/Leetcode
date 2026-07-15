77a7fc466fd8ac193457fc7659b29b77accddeb3
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-15 23:54:23 +0530

    Time: 1 ms (83.11%) | Memory: 42.5 MB (74.78%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,32 +1,16 @@
 class Solution {
+
+    //Recursively
+    // a = oddSum = n*n
+    // b = evenSum = n*(n+1)
+    
+    static int gcd(int a, int b){
+       if(b == 0) return a;
+       return gcd(b, a % b);
+    }
+
+
     public int gcdOfOddEvenSums(int n) {
-        // Odd sum: 1+3+5+... = n*n
-        // int oddSum = n * n;
-        
-        // Even sum: 2+4+6+... = n*(n+1)
-        // int evenSum = n * (n + 1);
-        
-
-        int oddSum = 0;
-        int evenSum = 0;
-
-        for(int i=1; i<=2*n-1; i+=2){
-            oddSum += i;
-        }
-
-        for(int i=2; i<=2*n; i+=2){
-            evenSum += i;
-        }
-
-
-        int min = Math.min(oddSum, evenSum);
-        int gcd = 1;
-        for(int i = min; i >= 1; i--) {  
-            if(oddSum % i == 0 && evenSum % i == 0) {
-                gcd = i;
-                break;
-            }
-        }
-        return gcd;
+        return gcd(n*n, n*(n+1));
     }
 }
