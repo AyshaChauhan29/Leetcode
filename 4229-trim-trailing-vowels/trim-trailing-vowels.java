626d1d7acd67b9a436402b82f10473fb4817492c
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-17 19:37:18 +0530

    Time: 1 ms (99.88%) | Memory: 44.5 MB (40.52%) - LeetSync

---
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String trimTrailingVowels(String s) {
+        int n = s.length();
+        int ans = 0;
+
+        for(int i=n-1; i>=0; i--){
+            char ch = s.charAt(i);
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                ans++;
+            }else{
+                break;
+            }
+        }
+        return s.substring(0, n-ans);
+    }
+}
