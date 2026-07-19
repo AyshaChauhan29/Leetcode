0b5c104ae00598aa10e2b3a9ac97c675e2adfff2
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-19 15:47:38 +0530

    Time: 50 ms (8.62%) | Memory: 47.9 MB (56.19%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxDistinct(String s) {
+        char[] ch = s.toCharArray();
+        int n = s.length();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int ele : ch){
+            map.put(ele, map.getOrDefault(ele, 0)+1);
+        }
+        return map.size();
+    }
+}
