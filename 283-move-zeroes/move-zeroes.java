6bfd050b85b72e5afb009d576e84a21ebf58afdf
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-23 19:41:06 +0530

    Time: 2 ms (92.13%) | Memory: 47.8 MB (57.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,29 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-    /*    int copy[] = new int[n];
-        
-        int j = 0;
-        for(int i=0; i<n; i++){
-            if(nums[i] != 0){
-               copy[j] = nums[i];
-               j++;
-            }
-        }
-        for(int i=0; i<n; i++){
-            nums[i] = copy[i];
-        }  
-                                     */
-    // stable using one pointer , no need of using two pointer
-         int left = 0;
 
-         for(int i=0; i<n; i++){
-            if(nums[i] != 0){
-                int temp = nums[left];
-                nums[left] = nums[i];
-                nums[i] = temp;
+        //TWO POINTERS APPROACH
+        int left = 0;
+
+        for(int right=0; right<n; right++){
+            if(nums[right] != 0){
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+
                 left++;
             }
-         }
+        }
     }
 }
