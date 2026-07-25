95b0891b81b1eddf7d894daff553ddc8c3bf8120
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-07-25 12:51:08 +0530

    Time: 2 ms (29.84%) | Memory: 42.9 MB (23.31%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -1,20 +1,32 @@
 class Solution {
     public int maxProduct(int n) {
+        //Brute Force Approach
+        // int maxProd = Integer.MIN_VALUE;
+        // int prod = 1;
 
-        int maxProd = Integer.MIN_VALUE;
-        int prod = 1;
+        // String s = Integer.toString(n);
+
+        // for(int i=0; i<s.length(); i++){
+        //     for(int j=i+1; j<s.length(); j++){
+        //        int num1 = s.charAt(i) - '0';
+        //        int num2 = s.charAt(j) - '0';
+
+        //        prod = num1 * num2;
+        //        maxProd = Math.max(maxProd, prod);
+        //     }   
+        // }    
+        // return maxProd;
+
+
+        //Sorting Approach
 
         String s = Integer.toString(n);
+        char[] ch = s.toCharArray();
+        Arrays.sort(ch);
 
-        for(int i=0; i<s.length(); i++){
-            for(int j=i+1; j<s.length(); j++){
-               int num1 = s.charAt(i) - '0';
-               int num2 = s.charAt(j) - '0';
+        int max = ch[ch.length-1] - '0';
+        int secMax = ch[ch.length-2] - '0';
 
-               prod = num1 * num2;
-               maxProd = Math.max(maxProd, prod);
-            }   
-        }    
-        return maxProd;
+        return max * secMax;
     }
 }
