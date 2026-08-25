e567b5b8eb0726181083df85c0055d45ab8c7666
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-08-25 13:20:22 +0530

    Time: 1 ms (4.18%) | Memory: 44.8 MB (14.08%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,17 +9,39 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        
-        while(curr != null){
-           ListNode next = curr.next;
 
-           curr.next = prev;
-           prev = curr;
-           curr = next;
+    ListNode dummy = new ListNode(0);
+    ListNode res = dummy;
+
+    public void createLL(int x){
+        res.next = new ListNode(x);
+        res = res.next;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ArrayList<Integer> li = new ArrayList<>();
+        ListNode temp = head;
+
+        while(temp != null){
+            li.add(temp.val);
+            temp = temp.next;
         }
-        return prev;
+
+        int low = 0;
+        int high = li.size()-1;
+
+        while(low <= high){
+            int tp = li.get(low);
+            li.set(low, li.get(high));
+            li.set(high, tp);
+
+            low++;
+            high--;
+        }
+
+        for(int i=0; i<li.size(); i++){
+            createLL(li.get(i));
+        }
+        return dummy.next;
     }
 }
