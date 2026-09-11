46de63f8befb5e40a45066a31ffea6644aa478fb
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-09-11 21:49:19 +0530

    Time: 1 ms (5.20%) | Memory: 44.9 MB (10.10%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -9,10 +9,18 @@
  * }
  */
 class Solution {
+    ListNode dummy = new ListNode(0);
+    ListNode res = dummy;
+
+    void createLL(int x){
+        res.next = new ListNode(x);
+        res = res.next;
+    }
+
+
     public ListNode rotateRight(ListNode head, int k) {
-
-        if(head == null || head.next == null) return head;    //if list is null/empty
-
+        if(head == null || head.next == null) return head;  //if list is null/empty
+        
         ArrayList<Integer> li = new ArrayList<>();
         ListNode temp = head;
 
@@ -25,19 +33,14 @@ class Solution {
 
         k = k % n;    // to perform only the leftover rotations
 
-        ListNode dummy = new ListNode(0);
-        ListNode res = dummy;
-
         // add last k elements
         for (int i = n - k; i < n; i++) {
-            res.next = new ListNode(li.get(i));
-            res = res.next;
+           createLL(li.get(i));
         }
 
         // Then add remaining elements
         for (int i = 0; i < n - k; i++) {
-            res.next = new ListNode(li.get(i));
-            res = res.next;
+           createLL(li.get(i));
         }
         return dummy.next;
     }
