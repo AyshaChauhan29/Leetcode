7c2a03c15efc01c377d608bde28e95e2dbdbbd50
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-09-14 20:11:47 +0530

    Time: 1 ms (100.00%) | Memory: 118.7 MB (80.87%) - LeetSync

---
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    static int count(ListNode head){
+        int count = 0;
+        while(head != null){
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+
+    ListNode dummy = new ListNode(0);
+    ListNode res = dummy;
+    
+    void createLL(int x) {
+        res.next = new ListNode(x);
+        res = res.next;
+    }
+
+    public ListNode swapNodes(ListNode head, int k) {
+    /*    ListNode temp = head;
+
+        int len = count(head);
+        int[] arr = new int[len];
+        int i=0;
+
+        while(temp != null){
+            arr[i] = temp.val;
+            i++;
+            temp = temp.next;
+        }
+
+        for(int j=0; j<len; j++){
+            int t = arr[k-1];
+            arr[k-1] = arr[len-k];
+            arr[len-k] = t;
+        }
+
+        for(int j=0; j<len; j++){
+            createLL(arr[j]);
+        }
+        return dummy.next;    */
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        for(int i=0; i<k-1; i++){
+            slow = slow.next;
+        }
+
+        ListNode first = slow;
+
+        while(slow.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        ListNode sec = fast;
+
+        int temp = first.val;
+        first.val = sec.val;
+        sec.val = temp;
+
+        return head;
+    }
+}
